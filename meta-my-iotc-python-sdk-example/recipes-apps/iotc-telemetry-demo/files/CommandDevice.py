df571bfd6d25062b37ec02f5bb5d93307d17a6fc
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-10 12:55:31 +0100

    Added initial commands support

---
+++ meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/CommandDevice.py
@@ -0,0 +1,53 @@
+from enum import Enum
+import sys
+from Enums import Enums as E
+from JsonDevice import JsonDevice
+sys.path.append("iotconnect")
+from typing import Union # to use Union[Enum, None] type hint
+
+import subprocess
+
+class CommandDevice(JsonDevice):
+    class DeviceCommands(Enum):
+        ECHO = "echo "
+        LED = "led "
+        TEST = "test_command"
+
+        @classmethod
+        def get(cls, full_command:str) -> Union[Enum, None]:
+            '''Validates full command string against accepted enumirated commands'''
+            if full_command is not None:
+                for dc in [dc.value for dc in cls]:
+                    if (sliced := full_command[:len(dc)]) == dc:
+                        return cls(sliced)
+            return None
+
+    def device_cb(self,msg):
+        # Only handles messages with E.Values.Commands.DEVICE_COMMAND (also known as CMDTYPE["DCOMM"])
+        full_command = E.get_value(msg, E.Keys.device_command)
+        hardcoded_command = self.DeviceCommands.get(full_command)
+
+        if hardcoded_command == self.DeviceCommands.ECHO:
+            to_print = full_command[len(self.DeviceCommands.ECHO.value):]
+            print(to_print)
+            self.send_ack(msg,E.Values.AckStat.SUCCESS, "Command Executed Successfully")
+
+        elif hardcoded_command == self.DeviceCommands.TEST:
+            self.send_ack(msg,E.Values.AckStat.SUCCESS, "Command Executed Successfully")
+
+        SCRIPTS_DIR = "/home/akarnil/Documents/Work/iotc-yocto-python-sdk/meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/"
+        command = full_command.split(' ')
+        command[0] = SCRIPTS_DIR + command[0]
+        process_result = subprocess.run(command, check=False, capture_output=True)
+
+        ack = E.Values.AckStat.FAIL
+        process_output_bytes: bytes = process_result.stderr
+
+        if process_result.returncode == 0:
+            ack = E.Values.AckStat.SUCCESS
+            process_output_bytes = process_result.stdout
+
+        ack_message = str(process_output_bytes, 'UTF-8')
+        self.send_ack(msg,ack, ack_message)
+
+
---
+++ meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/control_led.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+led_path="/home/akarnil/Documents/Work/iotc-yocto-python-sdk/meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/fake-led"
+
+# Check if the user provided an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <0 or 1>"
+    exit 1
+fi
+
+
+# Get the value from the command line argument
+value="$1"
+
+# Check if the provided value is either 0 or 1
+if [ "$value" -ne 0 ] && [ "$value" -ne 1 ]; then
+    echo "Error: Input must be either 0 or 1."
+    exit 1
+fi
+
+# Write the value to the hardcoded output file
+echo "$value" > "$led_path"
+
+# Check if the write was successful
+if [ $? -eq 0 ]; then
+    echo "Value '$value' written to '$led_path' successfully."
+else
+    echo "Error writing to '$led_path'."
+    exit 1
+fi
