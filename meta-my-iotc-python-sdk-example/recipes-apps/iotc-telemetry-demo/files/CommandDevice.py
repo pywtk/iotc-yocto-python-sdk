e7dc642fc5bc5ed27b6c6c95ecce92b09a6fd003
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-10 14:29:26 +0100

    script commands validated against existing scripts

--- meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/CommandDevice.py
+++ meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/CommandDevice.py
@@ -4,6 +4,7 @@ from Enums import Enums as E
 from JsonDevice import JsonDevice
 sys.path.append("iotconnect")
 from typing import Union # to use Union[Enum, None] type hint
+import os
 
 import subprocess
 
@@ -15,7 +16,7 @@ class CommandDevice(JsonDevice):
 
         @classmethod
         def get(cls, full_command:str) -> Union[Enum, None]:
-            '''Validates full command string against accepted enumirated commands'''
+            '''Validates full command string against accepted enumerated commands'''
             if full_command is not None:
                 for dc in [dc.value for dc in cls]:
                     if (sliced := full_command[:len(dc)]) == dc:
@@ -35,19 +36,30 @@ class CommandDevice(JsonDevice):
         elif hardcoded_command == self.DeviceCommands.TEST:
             self.send_ack(msg,E.Values.AckStat.SUCCESS, "Command Executed Successfully")
 
-        SCRIPTS_DIR = "/home/akarnil/Documents/Work/iotc-yocto-python-sdk/meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/"
-        command = full_command.split(' ')
-        command[0] = SCRIPTS_DIR + command[0]
-        process_result = subprocess.run(command, check=False, capture_output=True)
+        else:
+            command = full_command.split(' ')
+            
+            # Potentially move this variable to Json
+            SCRIPTS_DIR = "/home/akarnil/Documents/Work/iotc-yocto-python-sdk/meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/"
+            
+            # Potentially run once on start up
+            scripts: list = [f for f in os.listdir(SCRIPTS_DIR) if os.path.isfile(os.path.join(SCRIPTS_DIR, f))]
+            
+            if command[0] in scripts:
+                command[0] = SCRIPTS_DIR + command[0]
+                process_result = subprocess.run(command, check=False, capture_output=True)
 
-        ack = E.Values.AckStat.FAIL
-        process_output_bytes: bytes = process_result.stderr
+                ack = E.Values.AckStat.FAIL
+                process_output_bytes: bytes = process_result.stderr
 
-        if process_result.returncode == 0:
-            ack = E.Values.AckStat.SUCCESS
-            process_output_bytes = process_result.stdout
+                if process_result.returncode == 0:
+                    ack = E.Values.AckStat.SUCCESS
+                    process_output_bytes = process_result.stdout
 
-        ack_message = str(process_output_bytes, 'UTF-8')
-        self.send_ack(msg,ack, ack_message)
+                ack_message = str(process_output_bytes, 'UTF-8')
+                self.send_ack(msg,ack, ack_message)
+                return
+            
+            self.send_ack(msg,E.Values.AckStat.FAIL, "Command not valid/script not found")
 
 
