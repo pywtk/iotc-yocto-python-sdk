f1c20804e7e55c37136a152cfef48b9ad2ffb06d
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-12 17:30:29 +0100

    testing recursive file finding

--- meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/ota-payload-template/install.sh
+++ meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/ota-payload-template/install.sh
@@ -9,42 +9,44 @@ local_data_installed_dir="/usr/local/iotc/"
 application_backup_dir="${application_installed_dir::-1}.backup/"
 local_data_backup_dir="${local_data_installed_dir::-1}.backup/"
 
-tuples=("$application_installed_dir $application_backup_dir" "$local_data_installed_dir $local_data_backup_dir")
-for tuple in "${tuples[@]}"; do
-    eval "tuple=($tuple)"
-    installed_dir="${tuple[0]}"
-    backup_dir="${tuple[1]}"
+# tuples=("$application_installed_dir $application_backup_dir" "$local_data_installed_dir $local_data_backup_dir")
+# for tuple in "${tuples[@]}"; do
+#     eval "tuple=($tuple)"
+#     installed_dir="${tuple[0]}"
+#     backup_dir="${tuple[1]}"
 
-        if [ -d "$backup_dir" ]; then
-            echo "Removing existing backup... at $backup_dir"
-            rm -r "$backup_dir"
-        fi
+#         if [ -d "$backup_dir" ]; then
+#             echo "Removing existing backup... at $backup_dir"
+#             rm -r "$backup_dir"
+#         fi
 
-        echo "Creating backup directories at $backup_dir"
-        mkdir -p "$backup_dir"
+#         echo "Creating backup directories at $backup_dir"
+#         mkdir -p "$backup_dir"
 
-        echo "backing up  $installed_dir to $backup_dir"
-        cp -r "$installed_dir" "$backup_dir"
-done
-
-# # Initialize an empty array to store the file names
-# file_array=()
-
-# # Check if the folder exists
-# if [ -d "$folder" ]; then
-#     # Use a for loop to iterate over the files in the folder
-#     for file in "$folder"/*; do
-#         # Add the file to the array
-#         file_array+=("$file")
-#     done
-# else
-#     echo "Folder not found."
-# fi
-
-# # Display the list of files in the array
-# for file in "${file_array[@]}"; do
-#     echo "$file"
+#         echo "backing up  $installed_dir to $backup_dir"
+#         cp -r "$installed_dir" "$backup_dir"
 # done
+
+# Initialize an empty array to store the file names
+file_array=()
+
+# Check if the folder exists
+folder=$application_payload_dir
+if [ -d "$folder" ]; then
+    # Use the find command to search for files (not directories) recursively
+    while IFS= read -r -d '' file; do
+        if [ ! -d "$file" ]; then
+            file_array+=("$file")
+        fi
+    done < <(find "$folder" -type f -print0)
+else
+    echo "Folder not found."
+fi
+
+# Display the list of files in the array
+for file in "${file_array[@]}"; do
+    echo "$file"
+done
 
 
 # # Check if the source file exists
