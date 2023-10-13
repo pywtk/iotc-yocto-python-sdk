f717190b01760490afbef6e44e56da240f1da2d3
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-13 15:07:38 +0100

    update iotc-application.sh

--- meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/iotc-application.sh
+++ meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/iotc-application.sh
@@ -7,17 +7,34 @@ config_A_dir="/usr/local/iotc/"
 app_B_dir="${app_A_dir::-1}.backup/"
 config_B_dir="${config_A_dir::-1}.backup/"
 
-echo $app_A_dir$app_name
-echo $config_A_dir$config_name
+run_app() {
+    /usr/bin/python3 -u $1 $2
+}
 
-# Execute the first program
-/usr/bin/python3 -u $app_A_dir$app_name $config_A_dir$config_name
+SECONDARY_EXISTS=false
+SECONDARY_PATH=$app_B_dir$app_name
+if [ -e "${SECONDARY_PATH}" ]; then
+    SECONDARY_EXISTS=true
+fi
 
-# Check the exit status of the first program
-if [ $? -ne 0 ]; then
-    echo "The first program failed."
-    if [-d "$app_B_dir" ]; then
-        echo "Executing backup program"
-        /usr/bin/python3 -u $app_B_dir$app_name $config_B_dir$config_name
-    fi
+PRIMARY_EXISTS=false
+PRIMARY_PATH=$app_A_dir$app_name
+if [ -e "${PRIMARY_PATH}" ]; then
+    PRIMARY_EXISTS=true
+fi
+
+if [ "${PRIMARY_EXISTS}"] && [ "${SECONDARY_EXISTS}" ]; then
+    echo "Primary and Secondary exist, running"
+    run_app $app_A_dir$app_name $config_A_dir$config_name || (echo "Primary app has failed, trying backup" && run_app $app_B_dir$app_name $config_B_dir$config_name)
+
+elif [ "${PRIMARY_EXISTS}" ]; then
+    echo "Only Primary exists, running"
+    run_app $app_A_dir$app_name $config_A_dir$config_name
+
+elif [ "${SECONDARY_EXISTS}" ]; then
+    echo "Only Secondary exists, running"
+    run_app $app_B_dir$app_name $config_B_dir$config_name 
+
+else
+    echo "No valid application exists to run"
 fi
