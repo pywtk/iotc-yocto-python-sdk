8c4b3a9ca50391762022ace26ca974a073b2ab11
akarnil <138698161+akarnil@users.noreply.github.com>
2023-10-16 16:30:06 +0100

    iotc-application.sh now recovers against bad OTA

--- meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/iotc-application.sh
+++ meta-my-iotc-python-sdk-example/recipes-apps/iotc-telemetry-demo/files/iotc-application.sh
@@ -11,6 +11,34 @@ run_app() {
     /usr/bin/python3 -u $1 $2
 }
 
+restore_backup_to_primary() {
+    rm -rf $app_A_dir
+    rm -rf $config_A_dir
+
+    if [ -d "$app_A_dir" ]; then
+        echo "Removing existing primary app at $app_A_dir"
+        rm -r "$app_A_dir"
+    fi
+
+    if [ -d "$config_A_dir" ]; then
+        echo "Removing existing primary config at $config_A_dir"
+        rm -r "$config_A_dir"
+    fi
+
+    echo "Setting backup as primary"
+    mkdir -p $app_A_dir && cp -a $app_B_dir. $app_A_dir 
+    mkdir -p $config_A_dir && cp -a $config_B_dir. $config_A_dir 
+
+}
+
+recover_primary()
+{
+    echo "Primary app has failed, recovering"
+    restore_backup_to_primary
+    echo "Running backup"
+    run_app $app_A_dir$app_name $config_A_dir$config_name
+}
+
 SECONDARY_EXISTS=false
 SECONDARY_PATH=$app_B_dir$app_name
 if [ -e "$SECONDARY_PATH" ]; then
@@ -25,7 +53,8 @@ fi
 
 if [ "$PRIMARY_EXISTS" = true ] && [ "$SECONDARY_EXISTS" = true ]; then
     echo "Primary and Secondary exist, running"
-    run_app $app_A_dir$app_name $config_A_dir$config_name || (echo "Primary app has failed, trying backup" && run_app $app_B_dir$app_name $config_B_dir$config_name)
+    run_app $app_A_dir$app_name $config_A_dir$config_name || recover_primary
+
 
 elif [ "$PRIMARY_EXISTS" = true ]; then
     echo "Only Primary exists, running"
